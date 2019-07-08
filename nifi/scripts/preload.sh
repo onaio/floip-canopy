8be4379b2f9a946a07c9590724fde7f1dc6a50a5
nateMJ <natemaritim@gmail.com>
2019-07-08 11:35:04 +0300

    Add constraint to check if template exists before uploading

--- nifi/scripts/preload.sh
+++ nifi/scripts/preload.sh
@@ -45,22 +45,25 @@ initialize() {
 startFlows() {
   sleep 100
   echo "Starting flows"
+  # Check if template has been added to canvas
+  templates=$(curl http://nifi:8080/nifi-api/flow/templates | jq ".templates")
+  if [ "$templates"  == "[]" ] ;
+  then 
+    # Upload template and add it to canvas
+    curl -X POST -F template=@"/config/template/flow_results.xml" $NIFI_BASE_URL/nifi-api/process-groups/root/templates/upload
+    uploadedTemplateId=$(curl http://nifi:8080/nifi-api/flow/templates | jq ".templates[0].id" -r)
+    curl -X POST -H "Content-Type:application/json" $NIFI_BASE_URL/nifi-api/process-groups/root/template-instance -d '{"originX": 2.0,"originY": 3.0,"templateId": "'"$uploadedTemplateId"'"}' | jq ".flow.processGroups[0].id" -r
+    processorGroupId=$(curl -X GET $NIFI_BASE_URL/nifi-api/process-groups/root/process-groups | jq ".processGroups[0].id" -r)
 
-  # Upload template and add it to canvas
-  echo "$NIFI_BASE_URL/nifi-api/process-groups/root/templates/upload"
-  curl -X POST -F template=@"/config/template/flow_results.xml" $NIFI_BASE_URL/nifi-api/process-groups/root/templates/upload
-  uploadedTemplateId=$( curl http://nifi:8080/nifi-api/flow/templates | jq ".templates[0].id" -r)
-  curl -X POST -H "Content-Type:application/json" $NIFI_BASE_URL/nifi-api/process-groups/root/template-instance -d '{"originX": 2.0,"originY": 3.0,"templateId": "'"$uploadedTemplateId"'"}' | jq ".flow.processGroups[0].id" -r
-  processorGroupId=$(curl -X GET $NIFI_BASE_URL/nifi-api/process-groups/root/process-groups | jq ".processGroups[0].id" -r)
+    # Set DB password and start service
+    controllerServiceId=$(curl -X GET $NIFI_BASE_URL/nifi-api/flow/process-groups/$processorGroupId/controller-services | jq ".controllerServices[0].id" -r)
+    curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","properties":{"Password":"p@ssw0rd"}}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
+    curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","state":"ENABLED"}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
 
-  # Set DB password and start service
-  controllerServiceId=$(curl -X GET $NIFI_BASE_URL/nifi-api/flow/process-groups/$processorGroupId/controller-services | jq ".controllerServices[0].id" -r)
-  curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","properties":{"Password":"p@ssw0rd"}}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
-  curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","state":"ENABLED"}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
-
-  sleep 5
-  # Start the process group
-  curl -X PUT -H 'Content-Type: application/json' -d '{"id":"'"${processorGroupId}"'","state":"RUNNING"}' $NIFI_BASE_URL/nifi-api/flow/process-groups/${processorGroupId}
+    sleep 5
+    # Start the process group
+    curl -X PUT -H 'Content-Type: application/json' -d '{"id":"'"${processorGroupId}"'","state":"RUNNING"}' $NIFI_BASE_URL/nifi-api/flow/process-groups/${processorGroupId}
+    fi
 }
 
 main "$@" &
