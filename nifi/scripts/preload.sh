b887540384a5e879946a53fd9370d6023960632c
nateMJ <natemaritim@gmail.com>
2019-10-02 12:57:10 +0300

    Move flows to root process group

--- nifi/scripts/preload.sh
+++ nifi/scripts/preload.sh
@@ -53,16 +53,15 @@ startFlows() {
     curl -X POST -F template=@"/config/template/flow_results.xml" $NIFI_BASE_URL/nifi-api/process-groups/root/templates/upload
     uploadedTemplateId=$(curl http://nifi:8080/nifi-api/flow/templates | jq ".templates[0].id" -r)
     curl -X POST -H "Content-Type:application/json" $NIFI_BASE_URL/nifi-api/process-groups/root/template-instance -d '{"originX": 2.0,"originY": 3.0,"templateId": "'"$uploadedTemplateId"'"}' | jq ".flow.processGroups[0].id" -r
-    processorGroupId=$(curl -X GET $NIFI_BASE_URL/nifi-api/process-groups/root/process-groups | jq ".processGroups[0].id" -r)
 
     # Set DB password and start service
-    controllerServiceId=$(curl -X GET $NIFI_BASE_URL/nifi-api/flow/process-groups/$processorGroupId/controller-services | jq ".controllerServices[0].id" -r)
+    controllerServiceId=$(curl -X GET $NIFI_BASE_URL/nifi-api/flow/process-groups/root/controller-services | jq ".controllerServices[0].id" -r)
     curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","properties":{"Password":"p@ssw0rd"}}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
     curl -X PUT -H 'Content-Type: application/json' -d '{"revision":{"clientId":"random", "version":"0"},"component":{"id":"'"${controllerServiceId}"'","state":"ENABLED"}}' $NIFI_BASE_URL/nifi-api/controller-services/${controllerServiceId}
 
     sleep 5
     # Start the process group
-    curl -X PUT -H 'Content-Type: application/json' -d '{"id":"'"${processorGroupId}"'","state":"RUNNING"}' $NIFI_BASE_URL/nifi-api/flow/process-groups/${processorGroupId}
+    curl -X PUT -H 'Content-Type: application/json' -d '{"id":"root","state":"RUNNING"}' $NIFI_BASE_URL/nifi-api/flow/process-groups/root
     fi
 }
 
